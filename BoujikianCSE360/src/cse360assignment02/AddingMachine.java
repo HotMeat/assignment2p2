cb57623891726c3d7cb8618fb0b652a47b12486f
Ethan <Ethan@LAPTOP-9JT96UD4>
2020-10-05 20:36:29 -0700

    first assignment commit

---
+++ BoujikianCSE360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,52 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	/**
+	 * total variable
+	 */
+	 private int total;
+	  
+	 /**
+	  * adding method that sets total
+	  */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * getter or the total
+	   * @return a zero
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * add function
+	   * @param value
+	   */
+	  public void add (int value) {
+	  }
+
+	  /**
+	   * subtract function
+	   * @param value
+	   */
+	  public void subtract (int value) {
+	  }
+
+	  /**
+	   * string function
+	   * @return empty string
+	   */
+	  public String toString () {
+	    return "";
+	  }
+
+	  /**
+	   * clear function
+	   */
+	  public void clear() {
+	  }
+
+}
